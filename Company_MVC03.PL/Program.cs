ae53a8ad053d19ee1e803c0fb9790cf437639690
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-09-22 16:19:57 +0300

    Create Department Controller And Allow DI For All Services And Add Migration

--- Company_MVC03.PL/Program.cs
+++ Company_MVC03.PL/Program.cs
@@ -1,3 +1,8 @@
+using Company_MVC03.BLL.Interfaces;
+using Company_MVC03.BLL.Repositories;
+using Company_MVC03.DAL.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace Company_MVC03.PL
 {
     public class Program
@@ -7,7 +12,24 @@ namespace Company_MVC03.PL
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews(); // Register Built-in MVC Services
+            // V07
+            builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>(); // Allow DI For DepartmentRepository (Manually)
+            //V08
+            builder.Services.AddDbContext<CompanyDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            // V07
+
+            /*
+            builder.Services.AddDbContext<CompanyDbContext>(options =>
+            {
+
+                options.UseSqlServer("Server= PC_2001\\SQLEXPRESS; Database=CompanyDB03;Trusted_Connection=True;TrustServerCertificate=True;");// ->  Because we may be changed in it in the future so we copy it to "appsettings.json"
+            });
+            */
+            // Allow DI For CompanyDbContext (Manually)
 
             var app = builder.Build();
 
@@ -31,6 +53,11 @@ namespace Company_MVC03.PL
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
+
+            // VO8 
+            // in Package Manager Console -> install-package Microsoft.EntityFrameworkCore.Tools
+            // Add-Migration "InitialCreate" -OutputDir Data/Migrations // to create Migration in Data/Migrations folder (Default Project must be DAL) in this line
+            // Update-Database // to create Database from Migration (Default Project must be DAL) in this line
         }
     }
 }

b78cc9f3190a7a1b60f18e90ed8602b3063a8552
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-09-25 00:11:33 +0300

    Create Update(Action-View)

--- Company_MVC03.PL/Controllers/DepartmentController.cs
+++ Company_MVC03.PL/Controllers/DepartmentController.cs
@@ -60,7 +60,7 @@ namespace Company_MVC03.PL.Controllers
                 var count = _departmentRepository.Add(department);
                 if (count > 0)
                 {
-                    return RedirectToAction("Index");
+                    return RedirectToAction(nameof(Index));
                 }
             }
 
@@ -80,5 +80,63 @@ namespace Company_MVC03.PL.Controllers
             });
             return View(department);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id is null) return BadRequest("Invalid Id"); //400
+            var department = _departmentRepository.Get(id.Value);
+            if (department is null) return NotFound(new
+            {
+                StatusCode = 404,
+                message = $"Department With Id : {id} is not found"
+            });
+            return View(department);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute] int id, Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != department.Id) return BadRequest(); //400
+
+                var count = _departmentRepository.Update(department);
+
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+            return View(department);
+        }
+
+        /*
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, UpdateDepartmentDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var department = new Department()
+                {
+                    Id = id,
+                    Name = model.Name,
+                    Code = model.Code,
+                    CreateAt = model.CreateAt,
+                };
+                var count = _departmentRepository.Update(department);
+
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+            return View(model);
+        }*/
     }
 }

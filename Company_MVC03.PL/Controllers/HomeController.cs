bbd1f005fe69ddbbac96772fbd8e69a77e3114c4
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-09-30 22:32:13 +0300

    Dependancy Injection[Scoped, Transient, Singleton] Services

--- Company_MVC03.PL/Controllers/HomeController.cs
+++ Company_MVC03.PL/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using System.Text;
 using Company_MVC03.PL.Models;
+using Company_MVC03.PL.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company_MVC03.PL.Controllers
@@ -7,10 +9,43 @@ namespace Company_MVC03.PL.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IScopedService scopedService01;
+        private readonly IScopedService scopedService02;
+        private readonly ITransientServices transientServices01;
+        private readonly ITransientServices transientServices02;
+        private readonly ISingletonServices singletonServices01;
+        private readonly ISingletonServices singletonServices02;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IScopedService scopedService01,
+            IScopedService scopedService02,
+            ITransientServices transientServices01,
+            ITransientServices transientServices02,
+            ISingletonServices singletonServices01,
+            ISingletonServices singletonServices02
+            )
         {
             _logger = logger;
+            this.scopedService01 = scopedService01;
+            this.scopedService02 = scopedService02;
+            this.transientServices01 = transientServices01;
+            this.transientServices02 = transientServices02;
+            this.singletonServices01 = singletonServices01;
+            this.singletonServices02 = singletonServices02;
+        }
+
+        public string TestLifeTime()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"scopedService01 :: {scopedService01.GetGuid()}\n");
+            builder.Append($"scopedService02 :: {scopedService02.GetGuid()}\n\n");
+            builder.Append($"transientServices01 :: {transientServices01.GetGuid()}\n");
+            builder.Append($"transientServices02 :: {transientServices02.GetGuid()}\n\n");
+            builder.Append($"singletonServices01 :: {singletonServices01.GetGuid()}\n");
+            builder.Append($"singletonServices02 :: {singletonServices02.GetGuid()}\n\n");
+
+            return builder.ToString();
         }
 
         public IActionResult Index()
---
+++ Company_MVC03.PL/Services/ScopedServices.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Company_MVC03.PL.Services
+{
+    public class ScopedServices : IScopedService
+    {
+        public ScopedServices()
+        {
+            Guid = Guid.NewGuid();
+        }
+        public Guid Guid { get; set; }
+
+        public string GetGuid()
+        {
+            return Guid.ToString();
+        }
+    }
+}

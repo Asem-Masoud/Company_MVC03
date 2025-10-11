d66de3c037cdaac3c185bdedaeb916e8418f0400
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-10-11 15:03:58 +0300

    Microsoft Identity Package - DAL [AddEntityTables]

---
+++ Company_MVC03.DAL/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company_MVC03.DAL.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LasttName { get; set; }
+        public bool IsAgree { get; set; }
+
+    }
+}
